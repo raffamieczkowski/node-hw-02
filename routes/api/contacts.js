77b7105ab2311b78f211d46cc689ed6b5a26f8b2
raffamieczkowski <raffa.mieczkowski@gmail.com>
2024-05-06 20:28:07 +0200

    bug to fix - no action PUT / GET itd.
    bug to fix - no action PUT / GET itd.

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -3,18 +3,27 @@ const Contact = require('../../contactModel');
 const mongoose = require('mongoose');
 const router = express.Router();
 
+router.get('/', async (req, res) => {
+  try {
+    const contacts = await Contact.find();
+    res.json(contacts);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
 router.get('/:contactId', async (req, res) => {
-    try {
-      const contact = await Contact.findById(mongoose.Types.ObjectId(req.params.contactId));
-      if (contact) {
-        res.json(contact);
-      } else {
-        res.status(404).json({ message: "Contact not found" });
-      }
-    } catch (err) {
-      res.status(500).json({ message: err.message });
+  try {
+    const contact = await Contact.findById(req.params.contactId);
+    if (contact) {
+      res.json(contact);
+    } else {
+      res.status(404).json({ message: "Contact not found" });
     }
-  });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
 
 router.post('/', async (req, res) => {
   const contact = new Contact(req.body);
